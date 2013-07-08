076c95398fb1e69585b940aa4cd8b5df52f881d2
Eugene Poltorakov <jslayer@gmail.com>
2013-07-08 17:11:01 +0300

    add some todo

--- src/pui/base.js
+++ src/pui/base.js
@@ -24,6 +24,7 @@ define(['pui/object', 'pui/array', 'pui/base'], function(O, A){
     }
 
     return {
+        //todo - add extensions support
         create : function(name, P, px, sx){
             var C, F;
 
